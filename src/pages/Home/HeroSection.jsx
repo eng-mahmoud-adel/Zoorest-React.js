260a425f6c3c39d6d439bbfd91b2e8cf99a40772
eng-mahmoud-adel <eng.mahmoud.adel.labib@gmail.com>
2020-03-23 16:56:41 +0200

    https://trello.com/c/k437rjNI/

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import HeroImage from '../../images/assets/Group 12@3x.png';
+import SmallButton from '../../components/Buttons/SmallButtons/SmallButton';
 
-const HeroSection = props => {
+const HeroSection = (props) => {
+    const {data}= props;
     return (
-        <div>
+        <div className= "row">
+            <div className= "col-md-6">
+                <h1 className= "title font-weight-bold">{data.title}</h1>
+                <div className= "wrapper">
+                    <p className= "desc">{data.description}</p>
+                    <img src= {data.image} alt="" />
+                </div>
+                <div className= "w-50">
+                    <SmallButton text= "Find the Nearest Doctors Now" color= "btn btn-info" size= "btn-sm" />
+                </div>
+            </div>
 
+            <div className= "col-md-6">
+                <img src= {HeroImage} alt= "" className= "img-fluid" />
+            </div>
         </div>
     );
 };
