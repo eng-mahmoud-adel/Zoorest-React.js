4625f15ec336c3abbef1513b305906615d20bba4
eng-mahmoud-adel <eng.mahmoud.adel.labib@gmail.com>
2020-04-06 12:10:55 +0200

    creating testimonials-section https://trello.com/c/p5aLLffM/

--- src/components/Cards/Testimonials/Testimonial.js
+++ src/components/Cards/Testimonials/Testimonial.js
@@ -4,12 +4,12 @@ import Card from "react-bootstrap/Card";
 const Testimonial = (props) => {
     const {testimonial}= props;
     return (
-        <Card className={`testimonial-card my-2 w-100 ${props.className ? props.className : ""}`}>
+        <Card className={`testimonial-card my-4 ${props.className ? props.className : ""}`}>
             <Card.Title className= "mx-auto mb-0">
                 <i className="fa fa-quote-right fa-2x" aria-hidden="true"></i>
             </Card.Title>
             <Card.Body className= "py-0">
-                <Card.Text>{testimonial ? testimonial.text.substring(0, 235) : ""}</Card.Text>
+                <Card.Text>{testimonial ? testimonial.text.substring(0, 220) : ""}</Card.Text>
             </Card.Body>
             <Card.Footer>
                 <p className= "mb-0">{testimonial ? testimonial.author_name : ""}</p>
--- src/pages/Home/Testimonials.jsx
+++ src/pages/Home/Testimonials.jsx
@@ -1,19 +1,41 @@
 import React, { Fragment } from 'react';
 import Testimonial from '../../components/Cards/Testimonials/Testimonial';
+import SlickSlider from "../../components/Helpers/SlickSlider";
 
 const Testimonials = (props) => {
     const {testimonials}= props;
-    
+    const slickSettings = {
+        rtl: false,//todo use variable when site localization is added
+        dots: true,
+        infinite: true,
+        arrows: false,
+        initialSlide: 0,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    };
     return (
         <Fragment>
-            <h1 className= "font-weight-bold text-center mb-5">Special quotes<br /> from our customer about us</h1>
-            <div className= "row">
-            {testimonials && testimonials.map((testimonial, index) => (
-                <div className= "col-md-3">
-                    <Testimonial testimonial= {testimonial} key= {index} />
-                </div>
-            ))}
-            </div>
+            <h1 className= "font-weight-bold text-center">Special quotes<br /> from our customer about us</h1>
+            
+                <SlickSlider settings={slickSettings}>
+                    {testimonials && testimonials.map((testimonial, index) => (
+                        <Testimonial testimonial= {testimonial} key= {index} />
+                    ))}
+                </SlickSlider>
+            
         </Fragment>
     );
 };
