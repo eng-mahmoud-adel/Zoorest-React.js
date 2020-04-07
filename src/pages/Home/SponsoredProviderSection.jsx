71f71c68832d9679f24478e058da1b4388a0336a
eng-mahmoud-adel <eng.mahmoud.adel.labib@gmail.com>
2020-04-07 15:30:55 +0200

    adjusting responsive in nearest-doctor-section https://trello.com/c/eQEIbLS0/

--- src/pages/Home/SponsoredProviderSection.jsx
+++ src/pages/Home/SponsoredProviderSection.jsx
@@ -1,32 +1,33 @@
 import React from 'react'
 import Button from '../../components/Buttons/Button/Button';
 import Provider from '../../components/Cards/Profiles/Provider';
+import { Link } from 'react-router-dom';
 
 const SponsoredProviderSection = (props) => {
     return (
         <div className="row p-3">
             <div className="col-lg-5 mb-4 mb-md-4">
-                <h1 className="title mb-5 font-weight-bold">Find the Nearest Doctor for your Pets in Seconds</h1>
-                <div className="wrapper">
+                <h1 className="title mb-5 mt-0 font-weight-bold">Find the Nearest Doctor for your Pets in Seconds</h1>
+                <div className="wrapper mb-4">
                     <h3 className="font-weight-bold">Book your appointment now through our website</h3>
                     <p>Audit Bureau of Circulations integrated the definition of this medium in its latest report. Legal
                         rights are</p>
                 </div>
-                <div className="wrapper">
+                <div className="wrapper mb-4">
                     <h3 className="font-weight-bold">Find out about the nearest clinics and see their evaluation before
                         you visit</h3>
                     <p>Audit Bureau of Circulations integrated the definition of this medium in its latest report. Legal
                         rights are</p>
                 </div>
-                <div className="w-50">
-                    <Button text="Find the Nearest Doctors Now" color="btn btn-info" size="btn-sm"/>
+                <div className="col-xl-7 col-lg-8 col-md-5 col-sm-6 pl-0">
+                    <Link to= "/doctor"><Button text="Find the Nearest Doctors Now" color="btn btn-info" size="btn-sm"/></Link>
                 </div>
             </div>
             <div className="col-lg-7 section-two">
                 <div className="row">
 
                     {props.providers && props.providers.map((provider, index) => (
-                        <div className="col-6 mt-sm-4 mb-3" key={index}>
+                        <div className="col-md-6 col-sm-12 mt-sm-4" key={index}>
                             <Provider provider={provider} className="shadow-lg" cardTitle="Helen Figueroa"
                                       cardText="Here, I focus on a range of items and features that we use in life without giving them"
                                       icon="fa fa-eye" number="2312 Views"/>
