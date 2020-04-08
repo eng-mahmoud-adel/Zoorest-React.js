a97913617091fbef3f08c1d00aad9d94a544593a
eng-mahmoud-adel <eng.mahmoud.adel.labib@gmail.com>
2020-04-08 15:46:02 +0200

    adding validation in singup-form https://trello.com/c/6WdA9VYT/

--- src/components/Forms/SignUpForm.js
+++ src/components/Forms/SignUpForm.js
@@ -5,120 +5,124 @@ import Button from '../Buttons/Button/Button';
 import SocialBtn from '../Buttons/CombinedButtons/SocialBtn';
 import MultiSelect from '../Inputs/MultiSelect';
 import DropFileWithButton from './DropFiles/DropFileWithButton';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { red } from '@material-ui/core/colors';
 
 const SignUpForm = (props) => {
-    const [value, setValue] = useState({
-        value: "",
-        valid: "",
-        invalid: "",
-        name: /[a-z]{5, }/gi,
-    })
-
-
-    const validation = (e) => {
-        const userValue= e.target.value;
-        value.name.test(userValue) ?
-        setValue({
-            value: userValue,
-            valid: "is-valid",
-        })
-        :
-        setValue({
-            value: userValue,
-            valid: "is-invalid",
-        })
-        
+    const handleFormSubmit = (values) => {
+        console.log(values);
     }
-
     return (
-        <form>
-            <div className="form-group signup-form">
-                <div className="text-right">
-                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                    <a href="#" onClick={props.onLoginClicked}>Login</a>
-                </div>
-                <h3 className="title text-center mt-3 mb-5 pt-0 font-weight-bold">Sign Up with Us</h3>
-                <div className="my-2 mx-auto col-md-10">
-                    <Size size="btn-group-lg" textOne="User" textTwo="Doctor" textThree="Clinic"/>
-                </div>
-                <div className="row justify-content-center">
-                    <div className="my-3 col-md-10">
-                        <label className= "font-weight-bold">Full Name</label>
-                        <BasicInput className= {`basic-input + ${value.valid}`} type="text" placeholder="name" value= {value.value} validation= {validation} />
-                    </div>
-                    <div className="mb-3 col-md-10">
-                        <label className= "font-weight-bold">Email Address</label>
-                        <BasicInput className="basic-input" type="email" placeholder="email" />
-                    </div>
-                    <div className="mb-3 col-md-10">
-                        <label className= "font-weight-bold">Phone Number</label>
-                        <BasicInput className="basic-input" type="text" placeholder="number" />
-                    </div>
-                    <div className="mb-3 col-md-10">
-                        <label className= "font-weight-bold">Additional Phone Number</label>
-                        <BasicInput className="basic-input" type="text" placeholder="number" />
-                    </div>
-                    <div className="mb-3 col-md-5">
-                        <label className= "font-weight-bold">Password</label>
-                        <BasicInput className="basic-input" type="password" placeholder="password" />
-                    </div>
-                    <div className="mb-3 col-md-5">
-                        <label className= "font-weight-bold">Confirm Password</label>
-                        <BasicInput className="basic-input" type="password" placeholder="password" />
-                    </div>
-                    {props.user &&
-                    <div className={props.user}>
-                        <div className="mb-3 col-md-5">
-                            <MultiSelect/>
+        <Formik 
+            initialValues= {{name: "", password: "", email: "", phone_number_one: "", phone_number_two: ""}}
+            onSubmit= {handleFormSubmit}
+            validationSchema= {Yup.object().shape({
+                name: Yup.string().min(5, 'Too Short!').max(50, 'Too Long!').required('This field is required.'),
+                password: Yup.string().min(5, 'Too Short!').max(20, 'Too Long!').required('This field is required.'),
+                phone_number_one: Yup.string().min(10, 'Too Short!').max(12, 'Too Long!').required('This field is required.'),
+                phone_number_two: Yup.string().min(10, 'Too Short!').max(12, 'Too Long!').required('This field is required.'),
+                email: Yup.string().email('Invalid email').required('This field is required.'),
+            })}
+            render= {({values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting, isValid}) => 
+                <form>
+                    <div className="form-group signup-form">
+                        <div className="text-right">
+                            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                            <a href="#" onClick={props.onLoginClicked}>Login</a>
                         </div>
-                        <div className="mb-3 col-md-5">
-                            <MultiSelect/>
+                        <h3 className="title text-center mt-3 mb-5 pt-0 font-weight-bold">Sign Up with Us</h3>
+                        <div className="my-2 mx-auto col-md-10">
+                            <Size size="btn-group-lg" textOne="User" textTwo="Doctor" textThree="Clinic"/>
                         </div>
-                        <div className="mb-3 col-md-10">
-                            <MultiSelect/>
+                        <div className="row justify-content-center">
+                            <div className="my-3 col-md-10">
+                                <label className= "font-weight-bold">Full Name</label>
+                                <BasicInput className= "basic-input" name= "name" type="text" value= {values.name} placeholder="name" handleChange= {handleChange} handleBlur= {handleBlur} />
+                                {errors.name && touched.name && <div style= {{color: "red"}}>{errors.name}</div>}
+                            </div>
+                            <div className="mb-3 col-md-10">
+                                <label className= "font-weight-bold">Email Address</label>
+                                <BasicInput className="basic-input" name= "email" type="email" value= {values.email} placeholder="email" handleChange= {handleChange} handleBlur= {handleBlur} />
+                                {errors.email && touched.email && <div style= {{color: "red"}}>{errors.email}</div>}
+                            </div>
+                            <div className="mb-3 col-md-10">
+                                <label className= "font-weight-bold">Phone Number</label>
+                                <BasicInput className="basic-input" name= "phone_number_one" type="text" value= {values.phone_number_one} placeholder="number" handleChange= {handleChange} handleBlur= {handleBlur} />
+                                {errors.phone_number_one && touched.phone_number_one && <div style= {{color: "red"}}>{errors.phone_number_one}</div>}
+                            </div>
+                            <div className="mb-3 col-md-10">
+                                <label className= "font-weight-bold">Additional Phone Number</label>
+                                <BasicInput className="basic-input" name= "phone_number_two" type="text" value= {values.phone_number_two} placeholder="number" handleChange= {handleChange} handleBlur= {handleBlur} />
+                                {errors.phone_number_two && touched.phone_number_two && <div style= {{color: "red"}}>{errors.phone_number_two}</div>}
+                            </div>
+                            <div className="mb-3 col-md-5">
+                                <label className= "font-weight-bold">Password</label>
+                                <BasicInput className="basic-input" name= "password" type="password" value= {values.password} placeholder="password" handleChange= {handleChange} handleBlur= {handleBlur} />
+                                {errors.password && touched.password && <div style= {{color: "red"}}>{errors.password}</div>}
+                            </div>
+                            <div className="mb-3 col-md-5">
+                                <label className= "font-weight-bold">Confirm Password</label>
+                                <BasicInput className="basic-input" name= "password" type="password" value= {values.password} placeholder="password" handleChange= {handleChange} handleBlur= {handleBlur} />
+                                {errors.password && touched.password && <div style= {{color: "red"}}>{errors.password}</div>}
+                            </div>
+                            {props.user &&
+                            <div className= "row justify-content-center w-100">
+                                <div className="mb-3 col-md-5">
+                                    <MultiSelect/>
+                                </div>
+                                <div className="mb-3 col-md-5">
+                                    <MultiSelect/>
+                                </div>
+                                <div className="mb-3 col-md-10">
+                                    <MultiSelect/>
+                                </div>
+                            </div>}
+                            {props.doctor &&
+                            <div className= "row justify-content-center w-100">
+                                <div className="mb-3 col-md-5">
+                                    <label className= "font-weight-bold">Official doctor name</label>
+                                    <BasicInput className="basic-input" name= "name" type="text" value= {values.name} placeholder="name" handleChange= {handleChange} handleBlur= {handleBlur} />
+                                    {errors.name && touched.name && <div style= {{color: "red"}}>{errors.name}</div>}
+                                </div>
+                                <div className="mb-3 col-md-5">
+                                    <label className= "font-weight-bold">Select your location</label>
+                                    <BasicInput className="input-icon-left" type="text" right_icon="fa fa-commenting-o fa-lg"
+                                        placeholder="Input name" />
+                                </div>
+                                <div className="mb-3 col-md-5">
+                                    <MultiSelect/>
+                                </div>
+                                <div className="mb-3 col-md-5">
+                                    <MultiSelect/>
+                                </div>
+                                <div className="mb-3 col-md-10">
+                                    <DropFileWithButton/>
+                                </div>
+                            </div>}
+                            <div className="mb-4 col-md-9">
+                                <Button text="Sign Up" color="btn btn-info" size="btn-sm" onClick= {handleSubmit} disabled= {!isValid || isSubmitting} />
+                            </div>
                         </div>
-                    </div>}
-                    {props.doctor &&
-                    <div className={props.doctor}>
-                        <div className="mb-3 col-md-5">
-                            <label className= "font-weight-bold">Official doctor name</label>
-                            <BasicInput className="basic-input" type="text" placeholder="name" />
+                        <div className="row justify-content-center">
+                            <span className="line col-md-3 mb-3"></span>
+                            <span className="line col-md-3 offset-1 mb-3"></span>
                         </div>
-                        <div className="mb-3 col-md-5">
-                            <label className= "font-weight-bold">Select your location</label>
-                            <BasicInput className="input-icon-left" type="text" right_icon="fa fa-commenting-o fa-lg"
-                                placeholder="Input name" />
+                        <div className="row justify-content-center">
+                            <div className="col-md-4 text-right pr-2">
+                                <SocialBtn size="btn-sm" color="btn btn-light" text="Login with Facebook"
+                                        social_icon="fa fa-facebook-official fa-lg"/>
+                            </div>
+                            <div className="col-md-4 pl-2">
+                                <SocialBtn size="btn-sm" color="btn btn-light" text="Login with Google"
+                                        social_icon="fa fa-google fa-lg"/>
+                            </div>
                         </div>
-                        <div className="mb-3 col-md-5">
-                            <MultiSelect/>
-                        </div>
-                        <div className="mb-3 col-md-5">
-                            <MultiSelect/>
-                        </div>
-                        <div className="mb-3 col-md-10">
-                            <DropFileWithButton/>
-                        </div>
-                    </div>}
-                    <div className="mb-4 col-md-9">
-                        <Button text="Sign Up" color="btn btn-info" size="btn-sm"/>
                     </div>
-                </div>
-                <div className="row justify-content-center">
-                    <span className="line col-md-3 mb-3"></span>
-                    <span className="line col-md-3 offset-1 mb-3"></span>
-                </div>
-                <div className="row justify-content-center">
-                    <div className="col-md-4 text-right pr-2">
-                        <SocialBtn size="btn-sm" color="btn btn-light" text="Login with Facebook"
-                                   social_icon="fa fa-facebook-official fa-lg"/>
-                    </div>
-                    <div className="col-md-4 pl-2">
-                        <SocialBtn size="btn-sm" color="btn btn-light" text="Login with Google"
-                                   social_icon="fa fa-google fa-lg"/>
-                    </div>
-                </div>
-            </div>
-        </form>
+                </form>
+            }
+        >
+        </Formik>
     )
 };
 export default SignUpForm;
