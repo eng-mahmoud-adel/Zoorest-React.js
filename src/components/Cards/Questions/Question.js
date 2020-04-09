abdb53e85a4b32d0fae75aa91e7e5f0697d2e345
eng-mahmoud-adel <eng.mahmoud.adel.labib@gmail.com>
2020-04-09 17:26:39 +0200

    adjusting respnsive in question-component https://trello.com/c/KDxOXCkw/

--- src/components/Cards/Questions/Question.js
+++ src/components/Cards/Questions/Question.js
@@ -15,20 +15,20 @@ const Question = (props) => {
                 <Card.Title>{model ? model.title : props.cardTitle}</Card.Title>
                 <Card.Text>{model ? model.description.substring(0, 150) : props.cardText}</Card.Text>
                 <div className="row mb-3">
-                    <div className="col-2">
+                    <div className="col-xl-3 col-md-4 col-sm-5 mb-1 mb-md-0">
                         <Tag className="tag-two" text="#Tag"/>
                     </div>
-                    <div className="col-2">
+                    <div className="col-xl-3 col-md-4 col-sm-5 mb-1 mb-md-0">
                         <Tag className="tag-one" text="#Tag"/>
                     </div>
-                    <div className="col-2">
+                    <div className="col-xl-3 col-md-4 col-sm-5 mb-1 mb-md-0">
                         <Tag className="tag-two" text="#Tag"/>
                     </div>
                 </div>
             </Card.Body>
             <Card.Footer className="pb-0">
-                <div className="row align-items-center">
-                    <div className="col">
+                <div className="row">
+                    <div className="col-xl-4 col-md-6 mb-3 mb-xl-0">
                         {model ?
                             <Avatar className="avatar-four" name={model.author.name} radius={70}
                                     image={model.author.image_url}/>
@@ -36,7 +36,7 @@ const Question = (props) => {
                             <Avatar className="avatar-four" name="Terry Williams" span="Manager" image={image}/>
                         }
                     </div>
-                    <div className="icons col">
+                    <div className="icons col-xl-4 col-md-6 mb-3 mb-xl-0">
                         <div className="row">
                             <div className="col-4">
 
@@ -55,10 +55,11 @@ const Question = (props) => {
                     </div>
 
                     {/*todo hide this button when in details page*/}
-                    <div className="col text-center">
+                    <div className="col-xl-4 col-md-6 text-center">
                         {!props.hide_add_answer &&
-                        <Link to={`/question/${model && model.id}`} className={"btn btn-info btn-xs text-white"}>Add
-                            Answer</Link>}
+                        <Link to={`/question/${model && model.id}`}>
+                            <Button text="Add Answer" variant={"info"} size="btn-sm"/>
+                        </Link>}
                     </div>
                 </div>
             </Card.Footer>
