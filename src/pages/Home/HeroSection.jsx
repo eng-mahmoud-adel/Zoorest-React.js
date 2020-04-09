b484351cfc5f5de90640570c0d087bcb88a3c38a
eng-mahmoud-adel <eng.mahmoud.adel.labib@gmail.com>
2020-04-09 14:34:49 +0200

    adjusting responsive in hero-section in homepage https://trello.com/c/k437rjNI/

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 import HeroImage from '../../images/assets/images/Group 12@3x.png';
 import Button from '../../components/Buttons/Button/Button';
 import LazyLoad from "react-lazyload";
-import Tag from "../../components/Tags/Tag";
 import { Link } from 'react-router-dom';
 
 const HeroSection = (props) => {
     const {data} = props;
     return (
         <div className="row">
-            <div className="col-md-6">
-                <h1 className="title font-weight-bold">{data.title}</h1>
+            <div className="col-lg-6">
+                <h1 className="title font-weight-bold mt-0">{data.title}</h1>
                 <div className="wrapper">
                     <p className="desc">{data.description}</p>
                     <img src={data.image} alt=""/>
                 </div>
-                <div className="w-50">
+                <div className="col-xl-6 col-lg-7 col-md-5 col-9 pl-0 mb-5">
                     <Link to= "/doctor"><Button text="Find the Nearest Doctors Now" color="btn btn-info" size="btn-sm" /></Link>
                 </div>
             </div>
 
-            <div className="col-md-6">
+            <div className="col-lg-6">
                 <LazyLoad unmountIfInvisible={true} once={true}
                           placeholder={<h5 className="lazy loading">loading...</h5>}>
                     <img src={HeroImage} alt="" className="img-fluid"/>
