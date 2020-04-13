6c44d804a48a2f4799ad0adddc9e42894bf09190
eng-mahmoud-adel <eng.mahmoud.adel.labib@gmail.com>
2020-04-13 16:54:07 +0200

    adjusting responsive in footer https://trello.com/c/IEfDO3qh/

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ import BaseNavbar from "react-bootstrap/Navbar";
 
 const Footer = (props) => {
     return (
-        <footer className= "footer">
+        <footer className= "footer pb-5">
             <div className= "container">
                 <BaseNavbar expand="md" bg="faded" className="justify-content-center">
-                    <BaseNavbar.Brand href="/" className={"d-flex w-50 mr-auto"}>
+                    <BaseNavbar.Brand href="/" className={"d-flex w-50 mr-auto mx-auto justify-content-center"}>
                         <img
                             src={logo}
                             width={100}
@@ -17,46 +17,43 @@ const Footer = (props) => {
                             alt="zoorest brand logo"
                         />
                     </BaseNavbar.Brand>
-                    <BaseNavbar.Toggle aria-controls="collapsingNavbar"/>
-                    <BaseNavbar.Collapse className="w-100" id="collapsingNavbar">
-                        <Nav as="ul" className="navbar-nav w-100 justify-content-center">
-                            <Nav.Item as="li">
-                                <Link className="nav-link" to="/">Home</Link>
-                            </Nav.Item>
-                            <Nav.Item as="li">
-                                <Link className="nav-link" to="/question">Questions</Link>
-                            </Nav.Item>
-                            <Nav.Item as="li">
-                                <Link className="nav-link" to="/article">Articles</Link>
-                            </Nav.Item>
-                            <Nav.Item as="li">
-                                <Link className="nav-link find-doctor" to="/doctor">Find a doctor</Link>
-                            </Nav.Item>
-                            <Nav.Item as="li">
-                                <Link className="nav-link conditions" to="#">Terms &amp; Conditions</Link>
-                            </Nav.Item>
-                            <Nav.Item as="li">
-                                <Link className="nav-link policy" to="#">Privacy Policy</Link>
-                            </Nav.Item>
-                        </Nav>
-                        <ul className="navbar-nav w-25 justify-content-end social-links">
-                            <li className= "nav-item">
-                                <a className= "nav-link" href= "https://www.facebook.com/zoorestEG/" target= "_blank">
-                                    <i className="fa fa-facebook-official" aria-hidden="true"></i>
-                                </a>
-                            </li>
-                            <li className= "nav-item">
-                                <a className= "nav-link" href= "https://www.instagram.com/zoorest/" target= "_blank">
-                                    <i className="fa fa-instagram" aria-hidden="true"></i>
-                                </a>
-                            </li>
-                            <li className= "nav-item">
-                                <a className= "nav-link" href= "https://twitter.com/zoorest/" target= "_blank">
-                                    <i className="fa fa-twitter" aria-hidden="true"></i>
-                                </a>
-                            </li>
-                        </ul>
-                    </BaseNavbar.Collapse>
+                    <Nav as="ul" className="navbar-nav w-100 justify-content-center text-center">
+                        <Nav.Item as="li">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </Nav.Item>
+                        <Nav.Item as="li">
+                            <Link className="nav-link" to="/question">Questions</Link>
+                        </Nav.Item>
+                        <Nav.Item as="li">
+                            <Link className="nav-link" to="/article">Articles</Link>
+                        </Nav.Item>
+                        <Nav.Item as="li">
+                            <Link className="nav-link find-doctor" to="/doctor">Find a doctor</Link>
+                        </Nav.Item>
+                        <Nav.Item as="li">
+                            <Link className="nav-link conditions" to="#">Terms &amp; Conditions</Link>
+                        </Nav.Item>
+                        <Nav.Item as="li">
+                            <Link className="nav-link policy" to="#">Privacy Policy</Link>
+                        </Nav.Item>
+                    </Nav>
+                    <ul className="navbar-nav w-25 justify-content-md-end justify-content-between flex-row text-center social-links">
+                        <li className= "nav-item">
+                            <a className= "nav-link" href= "https://www.facebook.com/zoorestEG/" target= "_blank">
+                                <i className="fa fa-facebook-official" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li className= "nav-item">
+                            <a className= "nav-link" href= "https://www.instagram.com/zoorest/" target= "_blank">
+                                <i className="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li className= "nav-item">
+                            <a className= "nav-link" href= "https://twitter.com/zoorest/" target= "_blank">
+                                <i className="fa fa-twitter" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                    </ul>
                 </BaseNavbar>
                 <div className= "copy-right text-center">
                     All copyrights Reserved 2020. Zoorest&copy;
