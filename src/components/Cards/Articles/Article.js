21ae6f41b48293b0c43d2722b03db78e2348a1c5
eng-mahmoud-adel <eng.mahmoud.adel.labib@gmail.com>
2020-05-06 10:57:11 +0300

    using alloy editor to create new article component https://trello.com/c/vmhF8TCE/

--- src/components/Cards/Articles/Article.js
+++ src/components/Cards/Articles/Article.js
@@ -9,25 +9,53 @@ import {Text12, Text14Regular, Text24} from "../../UI/Typography";
 import {showModal} from "../../../store/actions/modal";
 import {connect} from "react-redux";
 import ClampLines from 'react-clamp-lines';
+import {EllipsisIcon} from '../../Icons/index';
 
 const Article = ({model: article, currentLocale}) => {
 
+    const showDropdown = () => {
+        document.getElementById(`article${article.id}`).classList.toggle("show");
+
+        // Close the dropdown if the user clicks outside of it
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+                let dropdowns = document.getElementsByClassName("dropdown-content");
+                for (let i = 0; i < dropdowns.length; i++) {
+                    let openDropdown = dropdowns[i];
+                    console.log(openDropdown)
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+        }
+
     return (
         <Card className="article-card m-1">
-            <Card.Title>
+            <div className="d-flex">
+                <Card.Title>
+                    <Text24>
+                        <ClampLines
+                            text={article.getLocalizedTitle(currentLocale)}
+                            id={`recent-article-title-${article.getKey()}`}
+                            lines={2}
+                            ellipsis="..."
+                            buttons={false}
+                            className="m-0"
+                            innerElement="div"
+                        />
+                    </Text24>
+                </Card.Title>
 
-                <Text24>
-                    <ClampLines
-                        text={article.getLocalizedTitle(currentLocale)}
-                        id={`recent-article-title-${article.getKey()}`}
-                        lines={2}
-                        ellipsis="..."
-                        buttons={false}
-                        className="m-0"
-                        innerElement="div"
-                    />
-                </Text24>
-            </Card.Title>
+                <EllipsisIcon className="ml-auto dropbtn mt-3 mr-3" style={{cursor: "pointer"}} onClick={showDropdown}/>
+                    <div id={`article${article.id}`} className="dropdown-content">
+                        <a href="#edit">Edit</a>
+                        <a href="#remove">Remove</a>
+                        <a href="#report">Report</a>
+                    </div>
+
+            </div>
             <small className="small-text">
                 <Text12 className="created-at">{article.humanizedCreatedAt()}</Text12>
             </small>
--- src/components/Cards/Questions/Question.js
+++ src/components/Cards/Questions/Question.js
@@ -29,12 +29,17 @@ const Question = ({model, className, hide_add_answer, currentLocale = "ar", like
         // Close the dropdown if the user clicks outside of it
         window.onclick = function(event) {
             if (!event.target.matches('.dropbtn')) {
-                if (document.getElementById(`question_${model.id}`).classList.contains('show')) {
-                    document.getElementById(`question_${model.id}`).classList.remove('show');
+                let dropdowns = document.getElementsByClassName("dropdown-content");
+                for (let i = 0; i < dropdowns.length; i++) {
+                    let openDropdown = dropdowns[i];
+                    console.log(openDropdown)
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
                 }
             }
         }
-    }
+        }
 
     return (
         <Card className={`question-card my-2 w-100 ${className || ""}`}>
