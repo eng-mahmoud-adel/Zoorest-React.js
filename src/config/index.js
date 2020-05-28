9dbf8fb9ec57b59b0ff8e9c0df4b731ee9b52d29
rami <Rami.amro.ahmed@gmail.com>
2020-05-28 17:40:17 +0200

    preparing staging environment
    Signed-off-by: rami <Rami.amro.ahmed@gmail.com>
    
    Took 59 minutes

--- src/config/index.js
+++ src/config/index.js
@@ -2,4 +2,7 @@ export default {
     apiUrl: 'https://dev.api.zoorest.com/api/v2/',
     fbAppId: '961048337408846',
     googleClientId: '924742163414-ds5occ9n2lp7hl2f3skemotibeo3jn45.apps.googleusercontent.com',
+    // apiUrl: process.env.API_ENDPOINT,
+    // fbAppId: process.env.FACEBOOK_APP_ID,
+    // googleClientId: process.env.GOOGLE_CLIENT_ID,
 }
