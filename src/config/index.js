f5edf4edc8262049815804b867da7ec2e0e731f6
rami <Rami.amro.ahmed@gmail.com>
2020-06-06 16:45:49 +0200

    fix staging link
    Signed-off-by: rami <Rami.amro.ahmed@gmail.com>
    
    Took 4 minutes

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
     // apiUrl: 'https://dev.api.zoorest.com/api/v2/',
-    apiUrl: 'https://dev.api.zoorest.com/api/v2/',
+    apiUrl: 'https://staging.backend.zoorest.com/api/v2/',
     fbAppId: '961048337408846',
     googleClientId: '924742163414-ds5occ9n2lp7hl2f3skemotibeo3jn45.apps.googleusercontent.com',
     // apiUrl: process.env.API_ENDPOINT,
